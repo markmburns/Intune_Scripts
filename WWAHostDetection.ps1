f9b9b957d57cb464c906a3ca7497504177b7cd52
markmburns <88446677+markmburns@users.noreply.github.com>
2021-12-22 16:27:47 +0000

    Update WWAHostDetection.ps1

--- WWAHostDetection.ps1
+++ WWAHostDetection.ps1
@@ -1,4 +1,14 @@
-﻿$ESPProcesses = Get-Process -Name 'wwahost' -ErrorAction 'SilentlyContinue'
+<# 
+    WWAHostDetection.ps1
+    mark.burns@dell.com
+
+    Custom requirement rule for Win32 applications
+    Use for apps incompatible with Autopilot ESP, such as those requiring user interaction or reconfiguring network
+    Add as additional script requirement with string output equals WWAHost is not running
+    Applications will be marked not-applicable until next boot, and will not break ESP
+
+#>
+$ESPProcesses = Get-Process -Name 'wwahost' -ErrorAction 'SilentlyContinue'
 if ($ESPProcesses.Count -eq 0) {
     Write-Host 'WWAHost is not running'
 }
