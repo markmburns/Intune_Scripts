a9ef7e7007c5b259eca1aedb7291a00b2186c68e
markmburns <88446677+markmburns@users.noreply.github.com>
2023-02-09 10:28:01 +0000

    Add files via upload

---
+++ Extraneous_Certs_Detection.ps1
@@ -0,0 +1,62 @@
+#Detection to determine if unwanted machine certs exist
+#Only looking at certs from a specific template
+#mark_burns@dell.com
+$intendedTemplate = "PKCS"
+#$unwantedPrefix = "MMBB"
+$intendedSubject = "CN=$($env:COMPUTERNAME)"
+$validCertFound = $false
+$extraneousCertFound = $false
+$output = ""
+function Get-CertificateTemplateName($certificate)
+{
+    # The template name is stored in the Extension data. 
+    # If available, the best is the extension named "Certificate Template Name", since it contains the exact name.
+    $templateExt = $certificate.Extensions | Where-Object{ ( $_.Oid.FriendlyName -eq 'Certificate Template Name') } | Select-Object -First 1   
+    if($templateExt) {
+        return $templateExt.Format(1)
+    }
+    else {
+        # Our fallback option is the "Certificate Template Information" extension, it contains the name as part of a string like:
+        # "Template=Web Server v2(1.3.6.1.4.1.311.21.8.2499889.12054413.13650051.8431889.13164297.111.14326010.6783216)"
+        $templateExt = $certificate.Extensions | Where-Object{ ( $_.Oid.FriendlyName -eq 'Certificate Template Information') } | Select-Object -First 1   
+        if($templateExt) {
+            $information = $templateExt.Format(1)
+
+            # Extract just the template name in $Matches[1]
+            if($information -match "^Template=(.+)\([0-9\.]+\)") {
+                return $Matches[1]
+            } else {
+                # No regex match, just return the complete information then
+                return $information
+            }
+        } else {
+            # No template name found
+            return $null
+        }
+    }
+}
+
+$myMachineCerts = Get-ChildItem -Path Cert:\LocalMachine\My
+foreach($cert IN $myMachineCerts){
+    $templateName =  Get-CertificateTemplateName $cert
+    If($templateName -match $intendedTemplate){
+        If($cert.Subject -match $intendedSubject){
+            #Write-Output "Valid certificate found: $($cert.Subject), $($templateName)"
+            $validCertFound = $true
+            $output = "$($output) | Valid: $($cert.Subject), $($templateName)"
+        }
+        If($cert.Subject -notmatch $intendedSubject){
+            #Write-Output "Extraneous certificate found: $($cert.Subject), $($templateName)"
+            $extraneousCertFound = $true
+            $output = "$($output) | Extraneous: $($cert.Subject), $($templateName)"
+        }
+    }
+}
+If($validCertFound -and $extraneousCertFound){
+    Write-Output "Valid and extraneous certs found.$($output)"
+    exit 1
+}else{
+    Write-Output "Valid and extraneous certs not found.$($output)"
+    exit 0
+}
+
