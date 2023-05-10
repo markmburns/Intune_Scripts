634d255faedede278b7420f827954911f18b28e8
markmburns <88446677+markmburns@users.noreply.github.com>
2023-05-10 13:59:36 +0100

    Add files via upload

---
+++ GroupTagSnippets.ps1
@@ -0,0 +1,7 @@
+﻿#Some PowerShell snippets for automating Group Tags
+Install-Module -name WindowsAutopilotIntune
+Connect-MSGraph
+
+get-autopilotdevice | where groupTag -EQ "" | Format-Table
+Get-AutopilotDevice | where {(($_.model -eq "Latitude 5421") -or ($_.model -eq "Latitude 5420")) -and ($_.groupTag -eq "")} | Format-Table
+Get-AutopilotDevice | where {(($_.model -eq "Latitude 5421") -or ($_.model -eq "Latitude 5420")) -and ($_.groupTag -eq "")} | Set-AutopilotDevice -groupTag "AAD"
